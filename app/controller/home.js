5449f914f65b748e14b988cf2fb82d124e0aa254
hardy <yincc1@yonyou.com>
2019-07-06 23:15:52 +0800

    增加配置

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,9 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    const data = await this.app.mysql.query('select * from agent'); // 单实例可以直接通过 app.mysql 访问
+    console.log(data)
+    ctx.body = data;
   }
 }
 
